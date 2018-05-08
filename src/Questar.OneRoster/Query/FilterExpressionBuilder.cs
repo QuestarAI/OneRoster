74e31f935458c96796e9ddde075b46559fb0355f
Josh Olson <Olson.dev@Gmail.com>
2018-05-08 01:39:56 -0500

    Handle single equal expressions.

--- src/Questar.OneRoster/Query/FilterExpressionBuilder.cs
+++ src/Questar.OneRoster/Query/FilterExpressionBuilder.cs
@@ -1,6 +1,7 @@
 namespace Questar.OneRoster.Query
 {
     using System;
+    using System.Linq;
     using System.Linq.Expressions;
 
     public class FilterExpressionBuilder<T>
@@ -10,8 +11,13 @@ namespace Questar.OneRoster.Query
         {
             if (string.IsNullOrWhiteSpace(filter)) return null;
             var filters = FilterParser.FromString(filter);
-
-            return null;
+            var first = filters.First();
+            var param = Expression.Parameter(typeof(T), "p0");
+            var prop = Expression.Property(param, first.FieldName);
+            var constant = Expression.Constant(first.Value);
+            var equal = Expression.Equal(prop, constant);
+            var func = Expression.Lambda<Func<T, bool>>(equal, false, param);
+            return func;
         }
     }
 }
--- src/Test/Questar.OneRoster.Test/FilterExpressionBuilderTest.cs
+++ src/Test/Questar.OneRoster.Test/FilterExpressionBuilderTest.cs
@@ -1,6 +1,8 @@
 namespace Questar.OneRoster.Test
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Linq.Expressions;
     using Mock;
     using Query;
@@ -14,6 +16,34 @@ namespace Questar.OneRoster.Test
             Expression<Func<Entity, bool>> expected = e => e.FooString == "42";
             var actual = FilterExpressionBuilder<Entity>.FromString("FooString='42'");
             Assert.True(ExpressionComparer.AreEqual(expected, actual));
+        }
+
+        [Fact]
+        public void CanApplyStringEqualExpression()
+        {
+            var predicate = FilterExpressionBuilder<Entity>.FromString("FooString='42'");
+            var list = new List<Entity>
+            {
+                new Entity { BarInt = 1, FooString = "Nope" },
+                new Entity { BarInt = 2, FooString = "42" },
+                new Entity { BarInt = 3, FooString = "9001" },
+                new Entity { BarInt = 4, FooString = "421" },
+                new Entity { BarInt = 5, FooString = "42" },
+                new Entity { BarInt = 6, FooString = "42 " },
+            };
+
+            var filtered = list.Where(predicate.Compile());
+
+            Assert.Collection(
+                filtered,
+                e =>
+                {
+                    Assert.Equal(2, e.BarInt);
+                },
+                e =>
+                {
+                    Assert.Equal(5, e.BarInt);
+                });
         }
     }
 }
