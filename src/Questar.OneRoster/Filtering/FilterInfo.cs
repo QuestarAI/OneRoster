4ccd6ed8f055c40336e352b58dcbbe592036050a
mattheiler <heiler.matthewd@gmail.com>
2018-11-20 22:06:36 -0600

    formatting

--- src/Questar.OneRoster/Filtering/FilterInfo.cs
+++ src/Questar.OneRoster/Filtering/FilterInfo.cs
@@ -6,12 +6,18 @@ namespace Questar.OneRoster.Filtering
     internal static class FilterInfo
     {
         internal static readonly MethodInfo Any
-            = typeof(Enumerable).GetMethods().Single(method => method.Name == nameof(Enumerable.Any) && method.GetParameters().Length == 2);
+            = typeof(Enumerable)
+                .GetMethods()
+                .Single(method => method.Name == nameof(Enumerable.Any) && method.GetParameters().Length == 2);
 
         internal static readonly MethodInfo All
-            = typeof(Enumerable).GetMethods().Single(method => method.Name == nameof(Enumerable.All) && method.GetParameters().Length == 2);
+            = typeof(Enumerable)
+                .GetMethods()
+                .Single(method => method.Name == nameof(Enumerable.All) && method.GetParameters().Length == 2);
 
         internal static readonly MethodInfo Contains
-            = typeof(Enumerable).GetMethods().Single(method => method.Name == nameof(Enumerable.Contains) && method.GetParameters().Length == 2);
+            = typeof(Enumerable)
+                .GetMethods()
+                .Single(method => method.Name == nameof(Enumerable.Contains) && method.GetParameters().Length == 2);
     }
 }
