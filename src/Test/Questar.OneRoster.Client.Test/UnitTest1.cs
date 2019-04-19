5bf4cc258af495207d4a1a5489603223b3c196f1
Matt Heiler <mheiler@questarai.com>
2019-04-19 10:04:52 -0500

    Moved test projects\files to the appropriate folder.

---
+++ src/Test/Questar.OneRoster.Client.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Questar.OneRoster.Client.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
