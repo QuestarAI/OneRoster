52772bb5ca152f911f59221a96aa051c527c8e31
Matt Heiler <mheiler@questarai.com>
2019-05-22 17:11:39 -0500

    fixed grade deserialization

--- src/Questar.OneRoster/Models/Grade.cs
+++ src/Questar.OneRoster/Models/Grade.cs
@@ -64,45 +64,6 @@ namespace Questar.OneRoster.Models
         [EnumMember(Value = "13")]
         ThirteenthGrade = 13,
 
-        [EnumMember(Value = "01")]
-        Grade1 = 1,
-
-        [EnumMember(Value = "02")]
-        Grade2 = 2,
-
-        [EnumMember(Value = "03")]
-        Grade3 = 3,
-
-        [EnumMember(Value = "04")]
-        Grade4 = 4,
-
-        [EnumMember(Value = "05")]
-        Grade5 = 5,
-
-        [EnumMember(Value = "06")]
-        Grade6 = 6,
-
-        [EnumMember(Value = "07")]
-        Grade7 = 7,
-
-        [EnumMember(Value = "08")]
-        Grade8 = 8,
-
-        [EnumMember(Value = "09")]
-        Grade9 = 9,
-
-        [EnumMember(Value = "10")]
-        Grade10 = 10,
-
-        [EnumMember(Value = "11")]
-        Grade11 = 11,
-
-        [EnumMember(Value = "12")]
-        Grade12 = 12,
-
-        [EnumMember(Value = "13")]
-        Grade13 = 13,
-
         [EnumMember(Value = "PS")]
         PostSecondary = 20,
 
